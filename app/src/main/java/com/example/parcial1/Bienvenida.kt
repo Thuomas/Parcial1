d96f13c08efd2337988e64e509bec7e3d167637e
Thuomas <Gastonmansilla9@gmail.com>
2024-05-07 20:55:47 -0300

    ultimo commit

--- app/src/main/java/com/example/parcial1/Bienvenida.kt
+++ app/src/main/java/com/example/parcial1/Bienvenida.kt
@@ -6,15 +6,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.example.parcial1.ui.theme.Parcial1Theme
 
 @Composable
 fun Bienvenida(modifier: Modifier = Modifier) {
+    val navControler = rememberNavController()
+    val currentBackStackEntry = navControler.currentBackStackEntry
+    
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
--- app/src/main/java/com/example/parcial1/MainPage.kt
+++ app/src/main/java/com/example/parcial1/MainPage.kt
@@ -5,20 +5,36 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.parcial1.ui.theme.Parcial1Theme
 
 @Composable
-fun MainPage( modifier: Modifier = Modifier) {
+fun MainPage(modifier: Modifier = Modifier) {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var contraseña by remember {
+        mutableStateOf("")
+    }
+    val navControler = rememberNavController()
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -29,7 +45,9 @@ fun MainPage( modifier: Modifier = Modifier) {
 
         TextField(
             value = "", // email
-            onValueChange = {}, // Ver que hace esta linea
+            onValueChange = {
+                email = it
+            }, // Ver que hace esta linea
             label = { Text("Correo electrónico") },
             modifier = Modifier.fillMaxWidth()
         )
@@ -38,7 +56,9 @@ fun MainPage( modifier: Modifier = Modifier) {
 
         TextField(
             value = "", // contraseña
-            onValueChange = {}, // Ver que hace esta linea
+            onValueChange = {
+                contraseña = it
+            },
             label = { Text("Contraseña") },
             modifier = Modifier.fillMaxWidth(),
             visualTransformation = PasswordVisualTransformation()
@@ -47,13 +67,32 @@ fun MainPage( modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(32.dp))
 
         Button(
-            onClick = {}, // hacer logica de boton
+            onClick = {
+                if (email == "pedro@pe.com.ar" && contraseña == "abc123") {
+                    navControler.navigate("Bienvenida/$email")
+                } else {
+                    //Mostrar mensaje de error
+                }
+
+            },
             modifier = Modifier.fillMaxWidth(0.5f)
         ) {
             Text("Iniciar sesión")
         }
     }
+
+        NavHost(
+            navController = navControler,
+            modifier = Modifier,
+            startDestination = "MainPage"
+        ) {
+            composable("Bienvenida") {
+                Bienvenida(navControler = navControler)
+            }
+        }
+
 }
+
 @Preview(showBackground = true)
 @Composable
 fun MainPagePreview() {
