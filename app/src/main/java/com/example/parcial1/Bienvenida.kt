c8fcb3960c076b91e86f4b3b4f9ce10d19b28d7a
Thuomas <Gastonmansilla9@gmail.com>
2024-05-07 20:19:45 -0300

    modifico pantalla
    Completo pantalla Bienvenida

--- app/src/main/java/com/example/parcial1/Bienvenida.kt
+++ app/src/main/java/com/example/parcial1/Bienvenida.kt
@@ -1,23 +1,39 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.parcial1.ui.theme.Parcial1Theme
 
 @Composable
-fun Bienvenida( modifier: Modifier = Modifier) {
+fun Bienvenida(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-    Text(text = "Hola")
+        Text(
+            text = "Bienvenido",
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Poner Email Usuario",
+            style = MaterialTheme.typography.titleSmall,
+            textAlign = TextAlign.Center
+        )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun BienvenidaPreview() {
